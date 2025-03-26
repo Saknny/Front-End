9c585e3a0885c7c09b798b1b2fd6019d59dd6955
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-03-26 15:46:35 +0200

    added drop down in navbar include logout email and change password

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,53 @@
 import "./Navbar.scss";
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { LoginContext } from "../../Context/Login/Login";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
   const { setIsAuthenticated } = useContext(LoginContext);
-
+  const userEmail = localStorage.getItem("email");
+  const navigate = useNavigate();
   function logOut() {
-    localStorage.removeItem("token");
+    localStorage.clear();
     setIsAuthenticated(false);
     toast.success("Logged out successfully");
   }
+
   return (
-    <nav className="navbar ps-4 pe-4 justify-content-end ">
-      <div className="right">
+    <nav className="navbar ps-4 pe-4 justify-content-end">
+      <div className="right dropdown">
         <img
           src="https://randomuser.me/api/portraits/men/1.jpg"
           alt="User"
-          className="profile "
+          className="profile dropdown-toggle"
+          id="userDropdown"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          style={{ cursor: "pointer" }}
         />
+        <ul
+          className="dropdown-menu dropdown-menu-end"
+          aria-labelledby="userDropdown"
+        >
+          <li>
+            <p className="dropdown-item text-muted">{userEmail}</p>
+          </li>
+          <li>
+            <button
+              onClick={() => navigate("/forgot-password")}
+              className="dropdown-item btn btn-primary"
+            >
+              Change Password
+            </button>
+          </li>
+          <li>
+            <button className="logout-btn" onClick={logOut}>
+              Logout
+              <i className="bx bx-log-out bx-rotate-180"></i>
+            </button>
+          </li>
+        </ul>
       </div>
-      <button className="logout-btn" onClick={logOut}>
-        <i className="bx bx-log-out bx-rotate-180"></i>
-      </button>
     </nav>
   );
 }
